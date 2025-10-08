623a9dfdb27dc89da6b232680b7bbdaa2075e4ce
Anas AARAB <121747499+Fletcher-Prog@users.noreply.github.com>
2025-10-08 16:04:39 +0200

    Create Q5.java

---
+++ Q5.java
@@ -0,0 +1,21 @@
+public class Q5 {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Veuille fournir un nombre N en argument.");
+            return;
+        }
+        int N;
+        try {
+            N = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            System.out.println("L'argument doit etre un nombre entier.");
+            return;
+        }
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
