cc11cf943bb0d697df645e60f51b7beabf7dbd37
Anas AARAB <121747499+Fletcher-Prog@users.noreply.github.com>
2025-10-08 16:09:15 +0200

    Résolurion Issue 1 Q5.java

--- Q5.java
+++ Q5.java
@@ -1,17 +1,17 @@
 public class Q5 {
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("Veuille fournir un nombre N en argument.");
+            System.out.println("Veuillez fournir un nombre N en argument.");
             return;
         }
         int N;
         try {
             N = Integer.parseInt(args[0]);
         } catch (NumberFormatException e) {
-            System.out.println("L'argument doit etre un nombre entier.");
+            System.out.println("L'argument doit être un nombre entier.");
             return;
         }
-        for (int i = 1; i <= N; i++) {
+        for (int i = N; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(j + " ");
             }
